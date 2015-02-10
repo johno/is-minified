cb760353671111977ff7191cc098aceeb96c0e8e
John Otander <johnotander@gmail.com>
2015-02-09 22:45:26 -0700

    Implement basic functionality, begin working on special comments

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict'
 
-module.exports = function isMinified(cssOrJsString) {
+module.exports = function isMinified(cssOrJsString, options) {
   if (typeof cssOrJsString !== 'string') {
     throw new TypeError('is-minified expects a string')
   }
 
-  return true
+  options = options || {}
+
+  var regex = options.ignoreSpecialComments ?
+    /(\s){2}|\n/ :
+    /(\/\*\!([\s\S]*?)\*\/)((\s){2}|\n)/
+
+  return !regex.test(cssOrJsString)
 }
--- test/test.js
+++ test/test.js
@@ -3,16 +3,24 @@ var isMinified = require('..')
 
 var minifiedStrings = [
   '.foobar{color:blue}',
-  'function(){return false}'
+  'function(){return false}',
+  '.foobar{ color:blue}'
 ]
 
 var nonMinifiedStrings = [
   '.foobar\n{color:blue}',
   '.foobar{color:blue  }',
   '.foobar{  color:blue  }',
-  '.foobar{\t\tcolor:blue  }',
+  '.foobar{\t\tcolor:blue     }',
   'function(){return false  }',
-  'function()  {return false}'
+  'function()  {return false}',
+  '/*!\n\n\   */   .foo{}',
+  '/*\n*/'
+]
+
+var specialCommentStrings = [
+  '.foo {} /*!\n\n\n*/',
+  '/*!\n\n \t\t \n\n*/function() {}'
 ]
 
 describe('is-minified', function() {
@@ -23,9 +31,29 @@ describe('is-minified', function() {
     })
   })
 
-  it('should return fase for non minified strings', function() {
+  it('should return false for non minified strings', function() {
     nonMinifiedStrings.forEach(function(cssOrJsString) {
       assert.ok(!isMinified(cssOrJsString))
+    })
+  })
+
+  describe('ignoreSpecialComments: true', function() {
+    it('should return true for minified strings', function() {
+      minifiedStrings.forEach(function(cssOrJsString) {
+        assert.ok(isMinified(cssOrJsString))
+      })
+    })
+
+    it('should return false for non minified strings', function() {
+      nonMinifiedStrings.forEach(function(cssOrJsString) {
+        assert.ok(!isMinified(cssOrJsString))
+      })
+    })
+
+    it('should ignore special comments', function() {
+      specialCommentStrings.forEach(function(cssOrJsString) {
+        assert.ok(isMinified(cssOrJsString, { ignoreSpecialComments: true }))
+      })
     })
   })
 })
