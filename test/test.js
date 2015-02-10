3e8ebbab84d82cbce277b4c30d78c8e45b77db01
John Otander <johnotander@gmail.com>
2015-02-09 21:58:22 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+var isMinified = require('..');
+
+describe('is-minified', function() {
+
+  it('should do something awesome', function() {
+    assert.equal(isMinified(), true);
+  });
+});
